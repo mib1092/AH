3dfc1b87d70a478c10eeed88afc9f14cf0cb2407
mdwork <mazdze@gmail.com>
2016-05-17 14:35:51 +0300

    commit

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  
+    // scroll navigation fixed
+    $(document).on("scroll", function(){
+        var mainNav = $('#navbar'),
+            headerHeight = $('.header-top-wrap').height();
+
+        console.log(headerHeight);
+
+        if($(window).scrollTop() > headerHeight && !mainNav.hasClass('active-nav')) {
+            mainNav.css({'position':'fixed', 'top':0, 'left':0});
+        }
+        else if($(window).scrollTop() <= headerHeight) {
+            mainNav.css({'position':'relative'});
+        }
+
+        $('.nav-wrap').css({'height': mainNav.height()});
+    });
+
 });
